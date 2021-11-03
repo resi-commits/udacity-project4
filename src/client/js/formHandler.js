6dbaa04ce1bb8b691f32066e4df9d4b7f2b3b27f
Theresa Adam <theresa.adam@daimler.com>
2021-11-03 09:44:07 +0100

    bring on same status as test project in lesson 4

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -3,14 +3,37 @@ function handleSubmit(event) {
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
-    checkForName(formText)
+    Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
-}
-
-export { handleSubmit }
+    getWeather(event).then(()=>{
+        updateUI();
+        })
+    }
+    
+    const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=71069&units=metric'
+    const apiKey = '&appid=fedf4384c03e6130b3fd1de93c0832b8'
+    
+    let d = new Date();
+    let newDate = d.getDate()+'.'+ (d.getMonth()+1)+'.'+ d.getFullYear();
+    
+    let data = [];
+    const getWeather = async (event) =>{
+        const res = await fetch(baseURL+apiKey)
+        try {
+          data = await res.json();
+          // console.log(data);
+        } catch(error) {
+          console.log("error", error);
+        }
+    }
+      
+    // async function to get all data stored in the projectData
+    const updateUI = async ()=>{
+        document.getElementById('results').innerHTML = data.main.temp;
+    
+    }
+    
+      
+    
+    export { handleSubmit }
--- webpack.dev.js
+++ webpack.dev.js
@@ -4,16 +4,29 @@ const HtmlWebPackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
+    devServer: {
+        hot: false,
+        inline: false
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.min.js',
+        libraryTarget: 'var',
+        library: 'Client'
+    },
     entry: './src/client/index.js',
     mode: 'development',
     devtool: 'source-map',
-    stats: 'verbose',
     module: {
         rules: [
             {
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
             }
         ]
     },
