5cc22de8ebf6e323785879035d3e48a1fcb1d218
Theresa Adam <theresa.adam@daimler.com>
2021-11-04 14:11:37 +0100

    add service workers and remove comment in index.js

--- src/server/index.js
+++ src/server/index.js
@@ -57,33 +57,6 @@ app.post('/meaning', async (req, res)=>{
     }
 });
 
-    
-// function requestMeaning(req, res){
-//     console.log("/meaning endpoint was requested")
-//     console.log(req.body.url)
-//     // took this from the meaningcloud API documentation: https://learn.meaningcloud.com/developer/sentiment-analysis/2.1/dev-tools
-//     const formdata = new FormData();
-//     formdata.append("key", apiKey);
-//     formdata.append("url", req.body.url);
-//     formdata.append("lang", "auto");
-
-//     const requestOptions = {
-//         method: 'POST',
-//         body: formdata,
-//         redirect: 'follow'
-//     };
-//     console.log(requestOptions)
-//     const resp = fetch("https://api.meaningcloud.com/sentiment-2.1", requestOptions)
-//     .then(resp => ({
-//         status: resp.status, 
-//         body: resp.json()
-//     }))
-//     .then(({ status, body }) => console.log(status, body))
-//     .catch(error => console.log('error', error));
-
-//     res.send(body)
-// }
-
 // designates what port the app will listen to for incoming requests
 app.listen(8081, function () {
     console.log('Example app listening on port 8081!')
